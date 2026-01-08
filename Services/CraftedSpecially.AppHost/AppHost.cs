4c05347804c93053706794c00d37c6b0ae2e8580
Tom van den Berg <tom.vandenberg@infosupport.com>
2026-01-08 20:06:47 +0100

    Stuff for health model session

--- Services/CraftedSpecially.AppHost/AppHost.cs
+++ Services/CraftedSpecially.AppHost/AppHost.cs
@@ -1,6 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject("Catalog", "../Catalog/Catalog.Api/Catalog.Api.csproj")
-    .WithEnvironment("APPLICATIONINSIGHTS_CONNECTION_STRING", "InstrumentationKey=388cc7a5-d1ce-4383-9a16-73a8dbdee67e;IngestionEndpoint=https://canadacentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://canadacentral.livediagnostics.monitor.azure.com/;ApplicationId=da52ab21-4f2b-4745-b55f-053a3d44df8b")
+    .WithEnvironment("APPLICATIONINSIGHTS_CONNECTION_STRING", "TODO")
     .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317");
 builder.Build().Run();
