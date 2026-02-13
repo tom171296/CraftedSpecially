0296f3f106d76a1c470050b1f5fb304224829bfd
Tom van den Berg <tom.vandenberg@infosupport.com>
2026-02-13 22:05:56 +0100

    Move to modular monolith setup

---
+++ src/modules/catalog/Catalog.UI/Endpoints.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Catalog.UI;
+
+public static class Endpoints
+{
+    public static IEndpointRouteBuilder MapCatalogEndpoints(this IEndpointRouteBuilder routes)
+    {
+        routes.MapGet("/", () => "Welcome to the Catalog.UI!");
+        return routes;
+    }
+}
